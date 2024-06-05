d2bb99bd1f499b81acb3cd78fc041cf11b4c99a4
askarullah <askarullaim@gmail.com>
2024-06-05 20:19:16 +0800

    Biodata

---
+++ Bio.py
@@ -0,0 +1,6 @@
+first_name = input("First Name: ")
+last_name = input("Last Name: ")
+address = input("Address: ")
+
+print("Name :  " + str(first_name) + str(last_name))
+print("Address :  " + address)
