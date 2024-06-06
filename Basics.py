bfc08c397eaa04f79a7255a8eecd73dffa130b64
askarullah <askarullaim@gmail.com>
2024-06-06 18:12:37 +0800

    Learn casting string to intrger

---
+++ Basics.py
@@ -0,0 +1,16 @@
+# Menampilkan output di terminal
+
+a = 3.14 #float
+x  = 3 #integer
+
+y = '3' # string
+
+c = a + x
+# d = y + x
+
+#casting
+print(type(y))
+y = int(y) #mengubah strinr jadi integer
+print(type(y))
+print(y+x) #sudah tidak error
+# print(d)
