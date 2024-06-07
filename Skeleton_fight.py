7906cf6316a310a10695668b5dc8e64606cab987
askarullah <askarullaim@gmail.com>
2024-06-07 20:04:46 +0800

    Aplying Icons In Python

---
+++ Skeleton_fight.py
@@ -0,0 +1,5 @@
+print("------ Welcome to the Cliffs And Caves ------ \n")
+print("Fight the Zombie And Skeleton Horde To get the coins! 🗡️ ")
+
+black_heart_icon = "\U0001F480" + "\U0001F5A4" * 5
+print(black_heart_icon)
