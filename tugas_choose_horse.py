8e47f3fc9f1f4034fde7689717db347f61907ae3
askarullah <askarullaim@gmail.com>
2024-06-11 22:36:34 +0800

    Challange Of Horses

---
+++ tugas_choose_horse.py
@@ -0,0 +1,10 @@
+print("------ Choose Your Horse ------")
+
+
+horse_gender = input("Enter Gender (male/female) : ")
+
+
+color = input("Enter Your Horse Color")
+print("")
+
+print("Congrats! \U0001F389, You Got A", horse_gender, "horse," "with", color, "color")
