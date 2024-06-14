a1f0b009686b062bd7194180c0222af590e6d921
askarullah <askarullaim@gmail.com>
2024-06-14 20:50:11 +0800

    naming convention

---
+++ learning_name_convention.py
@@ -0,0 +1,40 @@
+# flat case
+
+namefull = "poogieking3"
+print(namefull)
+
+# PascalCase
+
+
+PlantName = "Jupiter"
+print(PlantName)
+
+# snake_case
+
+
+first_score = "change 1"
+print(first_score)
+
+# UPPERFLATCASE
+
+
+CASEOH = "destruction"
+print(CASEOH)
+
+# Pascal_Snake_Case
+
+
+Close_The_Door = "STOP SAYING CLOSURE"
+print(Close_The_Door)
+
+# camel_Snake_Case
+
+
+too_Kid_Friendly = "Gd"
+print(too_Kid_Friendly)
+
+# SCREAMING_SNAKE_CASE
+
+
+GOOFY_AHH_SNAKE = "snake goofy"
+print(GOOFY_AHH_SNAKE)
